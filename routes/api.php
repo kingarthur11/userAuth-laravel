34e1991c94f542a2a3a39782d9113a253243183d
arthur orduh <arthurorduh@gmail.com>
2021-07-03 12:32:33 +0100

    update

--- routes/api.php
+++ routes/api.php
@@ -17,9 +17,9 @@ use App\Http\Controllers\API\ProductController;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
 
 
 Route::post('register', [RegisterController::class, 'register']);
