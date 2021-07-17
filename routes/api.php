9cef0b16be3b5c3c7ee6b26e725610f1f338cfad
arthur orduh <arthurorduh@gmail.com>
2021-07-17 09:16:59 +0100

    role based authorisation

--- routes/api.php
+++ routes/api.php
@@ -9,10 +9,8 @@ use App\Http\Controllers\API\ProductController;
 use App\Http\Controllers\API\DiaryController;
 use App\Http\Controllers\API\TransactionsController;
 
-Route::post('v1/register', [RegisterController::class, 'register']);
-Route::post('v1/login', [RegisterController::class, 'login'])->name('login');
-
-// Route::middleware('auth:api')->group( function () {
+    Route::post('v1/register', [RegisterController::class, 'register'])->name('register');
+    Route::post('v1/login', [RegisterController::class, 'login'])->name('login');
 
     Route::get('v1/users', [UserController::class, 'index']);
     Route::get('v1/user/{id}', [UserController::class, 'show']);
@@ -25,8 +23,6 @@ Route::middleware(['role', 'auth:api'])->group( function () {
     Route::get('v1/product/{id}', [ProductController::class, 'show']);
     Route::put('v1/product/{id}', [ProductController::class, 'update']);
     Route::delete('v1/product/{id}', [ProductController::class, 'destroy']);
-});
-
 
     Route::get('v1/diaries', [DiaryController::class, 'index']);
     Route::post('v1/diary', [DiaryController::class, 'store']);
@@ -36,38 +32,4 @@ Route::middleware(['role', 'auth:api'])->group( function () {
 
     Route::post('v1/credit', [TransactionsController::class, 'credit']);
     Route::post('v1/withdrawal', [TransactionsController::class, 'withdrawal']);
-// });
-
-
-
-// Route::middleware('auth:api')->group( function () {
-//     Route::get('user/{user}', [RegisterController::class, 'show']);
-//     Route::get('user/{user}/diaries', [RegisterController::class, 'show_diaries']);
-//     Route::get('v1/user/', [RegisterController::class, 'index']);
-//     Route::delete('v1/user/{user}', [RegisterController::class, 'destroy']);
-
-//     Route::get('user/{user}', [RegisterController::class, 'show']);
-//     Route::get('user/{user}/diaries', [RegisterController::class, 'show_diaries']);
-//     Route::get('v1/diaries/', [DiaryController::class, 'index']);
-//     Route::delete('v1/user/{user}', [RegisterController::class, 'destroy']);
-// });
-
-// Route::middleware('auth:api')->group( function () {
-//     Route::resource('v1/product', ProductController::class);
-//     Route::resource('v1/diary', ProductController::class);
-// });
-
-
-// Route::prefix(prefix: 'v1/user')->middleware('auth:api')->group( function () {
-//     Route::get('v1/user/{id}', [RegisterController::class, 'show']);
-//     Route::get('v1/user/', [RegisterController::class, 'index']);
-//     Route::delete('v1/user/{id}', [RegisterController::class, 'destroy']);
-
-//     Route::get('v1/diaries', [DiaryController::class, 'index']);
-//     Route::post('v1/diary', [DiaryController::class, 'store']);
-//     Route::get('v1/diary/{id}', [DiaryController::class, 'show']);
-//     Route::get('v1/diary/{id}/diaries', [DiaryController::class, 'update']);
-//     Route::delete('v1/diary/{id}', [DiaryController::class, 'destroy']);
-// });
-
-
+});
